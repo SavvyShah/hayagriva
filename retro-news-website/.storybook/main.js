5bb1061e69eaa704dab5f59632edf95443dd5cd8
Shubham <shubhamshahrising@gmail.com>
2020-10-08 18:16:21 +0530

    Setup storybook

---
+++ retro-news-website/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../client/src/stories/**/*.stories.mdx",
+    "../client/src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
